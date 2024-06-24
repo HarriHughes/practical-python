90bc078a8178fc229bbc209c9a91c656488f1552
HarriHughes <h.hughes@oxb.com>
2024-06-24 16:56:23 +0100

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,12 @@
 # mortgage.py
-#
+principal = 50000
+rate = 0.02
+payment = 1234
+totalPaid = 0.0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    totalPaid = totalPaid+ payment
+
+    print('total Paid', totalPaid)
 # Exercise 1.7
